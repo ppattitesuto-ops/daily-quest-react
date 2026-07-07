68f19793031f8816340bf7a301314a05f728cfd1
ppattitesuto-ops <ppattitesuto@gmail.com>
2026-07-07 21:15:37 +0900

    クエスト達成時の処理追加

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       }
     })
   };
-
+  // 要素の追加処理
   const handleAddQuest = () => {
     if (inputText.trim() === '') return;
     const newQuest = {
@@ -34,6 +34,17 @@ function App() {
     };
     setQuests([...quests, newQuest]);
     setInputText('');
+  };
+  // クエスト達成時の処理
+  const handleCompleteQuest = (id) => {
+    const updatedQuests = quests.map((quest) => {
+      if (quest.id === id) {
+        return { ...quest, isCompleted: true };
+      }
+      return quest;
+    });
+    setQuests(updatedQuests);
+    handleGainXp();
   };
 
   return (
@@ -61,10 +72,15 @@ function App() {
       <ul style={{ marginTop: '20px' }}>
         {/* ② 配列の中身をループして画面に<li>として出力する、JavaScriptの「配列用のメソッド名」は何？ */}
         {quests.map((quest) => (
-        <li key={quest.id} style={{margin: '5px 0'}}>
-          <input type="checkbox" checked={quest.isCompleted} readOnly />
-          <span style={{ marginLeft: '10px'}}>{quest.text}</span>
-        </li>
+          <li key={quest.id} style={{ margin: '5px 0' }}>
+            <input
+              type="checkbox"
+              checked={quest.isCompleted}
+              disabled={quest.isCompleted}
+              onChange={() => handleCompleteQuest(quest.id)}
+            />
+            <span style={{ marginLeft: '10px' }}>{quest.text}</span>
+          </li>
         ))}
       </ul>
     </div>

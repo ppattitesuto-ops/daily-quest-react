55b03d13cdb19c8afd227712cd654f6f7fc1e040
ppattitesuto-ops <ppattitesuto@gmail.com>
2026-07-12 15:55:43 +0900

    ♻️ ヘッダーの見た目をドラクエ風に

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,24 @@
 function Header({ level, xp, neededXp }) {
   return (
-    <div>
-      <div>現在のレベル: {level}</div>
-      <div>現在のXP: {xp} / 次のレベルまで: {neededXp}XP</div>
+    <div style={{
+      backgroundColor: '#000000',
+      border: '3px double #ffffff',
+      borderRadius: '0px',
+      padding: '15px',
+      width: '150px',
+      fontFamily: '"DotGothic16", sans-serif',
+      margin:'16px auto'
+    }}>
+      <div style={{ marginBottom: '10px', fontSize: '18px', letterSpacing: '2px' }}>
+        名前:???
+      </div>
+      <div style={{ fontSize: '14px', marginBottom: '5px' }}>
+        LV {level}
+      </div>
+      <div style={{ fontSize: '14px' }}>
+        XP {xp}
+      </div>
+      <div style={{ fontSize: '14px', color:'gray' }}>(NEXT LV: {neededXp}XP)</div>
     </div>
   );
 }
